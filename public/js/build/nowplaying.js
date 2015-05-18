77a70c96bc31eb7de97140efdb09f25d73692596
Barry Hoggard <barry@tristanmedia.com>
2015-05-18 01:00:02 -0400

    setting up react with jsx --watch

---
+++ public/js/build/nowplaying.js
@@ -0,0 +1,31 @@
+var Piece = React.createClass({displayName: "Piece",
+
+  getInitialState: function() {
+    return {
+      title: "", 
+      composer: ""
+    }
+  },
+
+  componentDidMount: function() {
+    $.get(this.props.source, function(result) {
+      this.setState({
+        title: result.title,
+        composer: result.composer
+      });
+    }.bind(this));   
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", null, 
+        React.createElement("h2", null, React.createElement("a", {target: "_blank", href: this.props.url}, this.props.name)), 
+        React.createElement("p", {className: "title"}, this.state.title), 
+        React.createElement("p", {className: "composer"}, this.state.composer)
+      )
+    );
+  }
+});
+
+React.render(React.createElement(Piece, {source: "/counterstream", name: "Counterstream Radio", url: "http://counterstreamradio.net/"}), 
+             document.getElementById('counterstream'));
---
+++ public/js/src/nowplaying.js
@@ -0,0 +1,31 @@
+var Piece = React.createClass({
+
+  getInitialState: function() {
+    return {
+      title: "", 
+      composer: ""
+    }
+  },
+
+  componentDidMount: function() {
+    $.get(this.props.source, function(result) {
+      this.setState({
+        title: result.title,
+        composer: result.composer
+      });
+    }.bind(this));   
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h2><a target="_blank" href={this.props.url}>{this.props.name}</a></h2>
+        <p className="title">{this.state.title}</p>
+        <p className="composer">{this.state.composer}</p>
+      </div>
+    );
+  }
+});
+
+React.render(<Piece source="/counterstream" name="Counterstream Radio" url="http://counterstreamradio.net/" />, 
+             document.getElementById('counterstream'));
